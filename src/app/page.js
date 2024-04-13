52e348d48f58fdab7a3d99a895d6c6540b686931
hasanarpat <black_the_dragon@hotmail.com>
2024-04-13 14:44:08 +0300

    change menuu bg and popup menu design

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className='w-full h-full'>
       <section className='flex flex-wrap items-center justify-center gap-6 w-[80%] mx-auto md:w-full'>
-        <div className='h-[580px] min-w-[300px] xl:min-w-[360px] bg-gray-300/30 rounded-2xl shadow-[0_10px_100px_rgba(0,0,0,.7)] relative pt-12 px-8 flex flex-col gap-10'>
+        <div className='h-[580px] min-w-[300px] xl:min-w-[360px] bg-gray-700/30 rounded-2xl shadow-[0_10px_100px_rgba(0,0,0,.7)] relative pt-12 px-8 flex flex-col gap-10'>
           <div className='absolute -top-3 left-0 right-0 w-[90%] mx-auto text-lg text-black bg-white rounded-xl flex items-center gap-2 p-2 py-0'>
             <span className='w-full py-1 text-justify px-2 text-[17px]'>
               <FaCalendarDay className='inline mr-1 text-xl' />
@@ -52,7 +52,7 @@ export default function Home() {
             description='Oyunu kapağından bil'
           />
         </div>
-        <div className='h-[580px] min-w-[300px] xl:min-w-[360px] bg-gray-300/30 rounded-2xl shadow-[0_10px_100px_rgba(0,0,0,.7)] relative pt-12 px-8 flex flex-col gap-10'>
+        <div className='h-[580px] min-w-[300px] xl:min-w-[360px] bg-gray-700/30 rounded-2xl shadow-[0_10px_100px_rgba(0,0,0,.7)] relative pt-12 px-8 flex flex-col gap-10'>
           <div className='absolute -top-3 left-0 right-0 w-[90%] mx-auto text-lg text-black bg-white rounded-xl flex items-center gap-2 p-2 py-0'>
             <span className='w-full py-1 text-justify px-2 text-[17px]'>
               <IoMdInfinite className='inline mr-1 text-xl' />
@@ -76,7 +76,7 @@ export default function Home() {
             background='game'
           />
         </div>
-        <div className='h-[580px] min-w-[300px] xl:min-w-[360px] bg-gray-300/30 rounded-2xl shadow-[0_10px_100px_rgba(0,0,0,.7)] relative pt-12 px-8 flex flex-col gap-10'>
+        <div className='h-[580px] min-w-[300px] xl:min-w-[360px] bg-gray-700/30 rounded-2xl shadow-[0_10px_100px_rgba(0,0,0,.7)] relative pt-12 px-8 flex flex-col gap-10'>
           <div className='absolute -top-3 left-0 right-0 w-[90%] mx-auto text-lg text-black bg-white rounded-xl flex items-center gap-2 p-2 py-0'>
             <span className='w-full py-1 text-justify px-2 text-[17px]'>
               <FaCalendarAlt className='inline mr-1 text-xl' />
--- src/components/layout/navbar/index.jsx
+++ src/components/layout/navbar/index.jsx
@@ -1,4 +1,5 @@
 'use client';
+import Link from 'next/link';
 import { useState } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 
@@ -8,11 +9,11 @@ export const Navbar = () => {
     <nav className='p-4  mx-auto w-full lg:w-2/3 h-16 mb-5'>
       <section className='flex items-center justify-between w-full'>
         <div></div>
-        <div className=''>
+        <Link href='/' className=''>
           <h1 className='bg-gradient-to-b from-orange-400 via-orange-600 to-orange-900 text-3xl uppercase inline-block text-transparent bg-clip-text hover:to-orange-400 hover:via-orange-400 drop-shadow-[0_3px_3px_rgba(0,0,0,1)]'>
             Gamuess
           </h1>
-        </div>
+        </Link>
         <div
           className='text-white text-lg hover:text-orange-400 cursor-pointer'
           onClick={() => setOpenMenu(true)}
@@ -21,7 +22,7 @@ export const Navbar = () => {
         </div>
         {openMenu && (
           <div className='absolute w-screen h-screen bg-opacity-50 bg-black top-0 left-0 z-50 flex items-center justify-center'>
-            <div className='w-1/3 min-h-64 bg-blue-800 flex flex-col items-center justify-center gap-px p-10 relative'>
+            <div className='max-w-xl w-full min-h-64 bg-blue-800 flex flex-col items-center justify-center gap-px px-4 py-4 relative'>
               <details className='w-full bg-purple-900 rounded-md rounded-b-none p-2 cursor-pointer text-orange-400'>
                 <summary>Merhaba</summary>
                 <div className=''>Şimdi görüyorsun</div>
